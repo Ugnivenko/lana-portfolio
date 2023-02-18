2d653f1b35ad149c4e9f78905943aaeb3f9884f4
Ugnivenko <110766390+Ugnivenko@users.noreply.github.com>
2023-02-18 13:13:26 +1000

    typing animation
    JS

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// This creates a new typing animation in vanilla javascript 
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 100,
+    backSpeed: 15,
+    smartBackspace: true,
+    loop: true,
+    loopCount: Infinity
+   });
